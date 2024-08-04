b6c0203190a731e3bbd763dab57777d583c0a730
Maikel Ordaz <maikelordaz@gmail.com>
2024-08-04 13:09:43 -0400

    airdrop

---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeErc20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract MerkleAirdrop is EIP712 {
+    using SafeERC20 for IERC20;
+
+    address[] claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+
+    mapping(address claimer => bool claimed) private s_hasClaimed;
+
+    bytes32 private constant MESSAGE_TYPEHASH =
+        keccak256("AirdropClaim(address account, uint256 amount)");
+
+    struct AirdropClaim {
+        address account;
+        uint256 amount;
+    }
+
+    event Claim(address account, uint256 amount);
+
+    error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
+    error MerkleAirdrop__InvalidSignature();
+
+    constructor(
+        bytes32 merkleRoot,
+        IERC20 airdropToken
+    ) EIP712("MerkleAirdrop", "1") {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claim(
+        address account,
+        uint256 amount,
+        bytes32[] calldata merkleProof,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        if (s_hasClaimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+        // Check the signature
+        if (
+            !_isValidSignature(
+                account,
+                getMessageHash(account, amount),
+                v,
+                r,
+                s
+            )
+        ) {
+            revert MerkleAirdrop__InvalidSignature();
+        }
+        // Calculate using the account and the amount, the hash -> leaf node
+        // We encode the values to be hashed together
+        // We hash it twice to avoid hash collisions, this is to avoid second pre-image attacks
+        // It can be done once, but the standard is to do it twice
+        bytes32 leaf = keccak256(
+            bytes.concat(keccak256(abi.encode(account, amount)))
+        );
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+
+        s_hasClaimed[account] = true;
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+    }
+
+    function getMessageHash(
+        address account,
+        uint256 amount
+    ) public view returns (bytes32) {
+        return
+            _hashTypedDataV4(
+                keccak256(
+                    abi.encode(
+                        MESSAGE_TYPEHASH,
+                        AirdropClaim({account: account, amount: amount})
+                    )
+                )
+            );
+    }
+
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
+
+    function _isValidSignature(
+        address account,
+        bytes32 digest,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) internal pure returns (bool) {
+        (address actualSigner, , ) = ECDSA.tryRecover(digest, v, r, s);
+        return actualSigner == account;
+    }
+}
